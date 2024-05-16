1591d672107e941837e893a731774ffd101c17e4
Denver <audgkwns44@gmail.com>
2024-05-16 13:43:25 +0900

    [Create]mockup signin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,34 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
+import { Dispatch, SetStateAction, createContext, useState } from "react";
+
+interface UserObject {
+  id: number;
+  name: string | null;
+}
+
+export const AuthContext = createContext<UserObject | null>(null);
+export const HandleAuthContext = createContext<Dispatch<
+  SetStateAction<UserObject>
+> | null>(null);
 
 function App() {
+  const [user, setUser] = useState<UserObject>({
+    id: -1,
+    name: null,
+  });
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-      </Routes>
-    </Router>
+    <AuthContext.Provider value={user}>
+      <HandleAuthContext.Provider value={setUser}>
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+          </Routes>
+        </Router>
+      </HandleAuthContext.Provider>
+    </AuthContext.Provider>
   );
 }
 
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -2,8 +2,28 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import LogoIcon from "../Components/Svgs/LogoIcon";
+import { useCallback, useContext, useEffect } from "react";
+import { AuthContext, HandleAuthContext } from "../App";
 
 export default function HomePage() {
+  const user = useContext(AuthContext);
+  const setUser = useContext(HandleAuthContext);
+
+  const signin = useCallback(() => {
+    if (setUser) {
+      setUser({
+        id: 1,
+        name: "nomad",
+      });
+    } else {
+      alert("Failed to signin");
+    }
+  }, [setUser]);
+
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
     <Box
       className="centralize"
@@ -18,10 +38,9 @@ export default function HomePage() {
       </Box>
       <Typography>Hello, Nomad Coder!</Typography>
       <Typography>이 페이지는 누구나 접근할 수 있습니다</Typography>
-
       <Button
         onClick={() => {
-          alert("hi");
+          signin();
         }}
       >
         Sign In
